ab7cd9e6c76db9f1bc3727d8669afd7f534b9d79
maxtermax <esnene02@gmail.com>
2018-02-13 21:19:26 -0500

    adding absolte path to index.html

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.NODE_ENV === 'development' ? 3000 : process.env.PORT;
+const path = require('path');
 
 app.get('/', function (req, res) {
-  res.sendFile('./index.html');
+  res.sendFile(path.resolve(__dirname, '/index.html'));
 });
 app.listen(port, function () {
   console.log('Example app listening on port 3000!');
