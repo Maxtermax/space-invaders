f7c10b67197062105c3d2e7d9baff5455a674cbc
maxtermax <esnene02@gmail.com>
2018-02-13 21:20:59 -0500

    fix path to index.html

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.NODE_ENV === 'development' ? 3000 : process.env.PORT;
 const path = require('path');
 
 app.get('/', function (req, res) {
-  res.sendFile(path.resolve(__dirname, '/index.html'));
+  res.sendFile(path.resolve(__dirname, './index.html'));
 });
 app.listen(port, function () {
   console.log('Example app listening on port 3000!');
