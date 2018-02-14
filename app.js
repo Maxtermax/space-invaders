d00afe00ea72f8d8f5b8d9b532656d97803ce2ef
maxtermax <esnene02@gmail.com>
2018-02-13 21:26:38 -0500

    adding static folder public

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-const port = process.env.NODE_ENV === 'development' ? 3000 : process.env.PORT;
+const PORT = process.env.NODE_ENV === 'production' ? process.env.PORT : 3000;
 const path = require('path');
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.resolve(__dirname, './index.html'));
 });
-app.listen(port, function () {
-  console.log('Example app listening on port 3000!');
+
+app.listen(PORT, function () {
+  console.log(`App listen on: ${PORT}`);
 });
