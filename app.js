600bc3ec97e1c8209c7a654d7248e0d27cd9954a
maxtermax <esnene02@gmail.com>
2018-03-06 16:10:17 -0500

    adding heroku port

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const PORT = 3009;//must run on this port to be allow run the service worker correctly
+const ENV = process.env.NODE_ENV;
+const PORT = ENV === "production" ? process.env.PORT : 3009;//must run on this port to be allow run the service worker correctly
 app.use(express.static(path.resolve(__dirname, './public')));
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, './index.html'));
